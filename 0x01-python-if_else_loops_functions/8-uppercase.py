22e0f4e0da3d77b409ec8fef7fe773bb50d893c1
Getaneh48 <getaneh11@gmail.com>
2022-11-29 16:04:04 +0300

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_letter(c, dlm):
+def p_letter(c, dlm):
     if ord(c) >= 97 and ord(c) <= 122:
         print("{}{}".format(chr(ord(c) - 32), dlm), end="")
     else:
@@ -9,6 +9,6 @@ def print_letter(c, dlm):
 def uppercase(str):
     for index, c in enumerate(str):
         if index == (len(str) - 1):
-            print_letter(c, "\n")
+            p_letter(c, "\n")
         else:
-            print_letter(c, "")
+            p_letter(c, "")
