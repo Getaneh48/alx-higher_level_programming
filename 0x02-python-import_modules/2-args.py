7e1a50799495f806848c75defc707031a75067b8
Getaneh48 <getaneh11@gmail.com>
2022-12-01 14:37:48 +0300

    pystyle fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,11 @@ if __name__ == "__main__":
 
     import sys
 
-
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) > 1:
         print("{} arguments:".format(len(sys.argv) - 1), end="\n")
         for index, i in enumerate(sys.argv):
             if index == 0:
-                 continue
+                continue
             print("{}: {}".format(index, sys.argv[index]), end="\n")
