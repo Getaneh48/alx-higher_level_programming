a482e4bd20f9fe8ce9baeaa5417c4435702107a3
Getaneh48 <getaneh11@gmail.com>
2022-12-01 14:51:06 +0300

    bug fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) > 1:
+    elif len(sys.argv) == 2:
+        print("1 argument:", end="\n")
+        print("1: {}".format(sys.argv[1]), end="\n")
+    elif len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1), end="\n")
         for index, i in enumerate(sys.argv):
             if index == 0:
