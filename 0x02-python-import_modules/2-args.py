15b3adfda4e7cc5784b2038f6411bd0ffdd54e4f
Getaneh48 <getaneh11@gmail.com>
2022-12-01 14:08:49 +0300

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1), end="\n")
+        for index, i in enumerate(sys.argv):
+            if index == 0:
+                 continue
+            print("{}: {}".format(index, sys.argv[index]), end="\n")
