a4f870701c12e3e51245ac3a9d39063720c30112
Getaneh48 <getaneh11@gmail.com>
2022-12-03 13:52:44 +0000

    Task 7 bug fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+def fill_t(t):
+    if len(t) == 0:
+        return (0, 0)
+    elif len(t) == 1:
+        return (t[0], 0)
+    else:
+        return (t)
+
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 0:
-        return (tuple_a)
-    elif len(tuple_b) == 1:
-        return ((tuple_a[0] + tuple_b[0], tuple_a[1]))
-    else:
+        tuple_a = fill_t(tuple_a)
+        tuple_b = fill_t(tuple_b)
         return ((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
