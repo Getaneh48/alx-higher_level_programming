00fdfeaa055b21c7adb4cf1d4db4223803d6bd24
Getaneh48 <getaneh11@gmail.com>
2022-12-04 09:36:32 +0000

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+
+
+def is_d(n):
+    if n % 2 == 0:
+        return (True)
+    else:
+        return (False)
+
+
+def divisible_by_2(my_list=[]):
+    return ([is_d(i) for i in my_list])
