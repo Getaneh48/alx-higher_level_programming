307905ebec2eaa2301e32fb7309af56e5667f239
Getaneh48 <getaneh11@gmail.com>
2022-12-05 11:29:39 +0300

    bug fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix[0]:
-        print("", end="\n")
-        return
     for m in matrix:
         for ind, i in enumerate(m):
             if ind < len(m) - 1:
                 print("{:d}".format(i), end=" ")
             else:
-                print("{:d}".format(i))
+                print("{:d}".format(i), end="")
+        print()
