c70306403fc58dafe94cfe637d5f749f9c55025d
Getaneh48 <getaneh11@gmail.com>
2022-12-07 14:19:51 +0300

    pycodestyle fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [list(map(lambda n : n ** 2, l)) for l in matrix]
+    new_matrix = [list(map(lambda n:n ** 2, lst)) for lst in matrix]
     return new_matrix
