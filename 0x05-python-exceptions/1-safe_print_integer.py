6361321a9c9db81171c50a10a2682e19a50ef886
Getaneh48 <getaneh11@gmail.com>
2022-12-19 14:41:30 +0300

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     status = 0
     try:
         if (isinstance(value, int)):
-            print("{}".format(value), end="\n")
+            print("{:d}".format(value), end="\n")
             status = 1
         else:
             raise TypeError
