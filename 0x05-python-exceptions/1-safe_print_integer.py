52d4131934f69e9d4f3bfe8924d7efbaa7f47b2d
Getaneh48 <getaneh11@gmail.com>
2022-12-19 14:39:58 +0300

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    status = 0
+    try:
+        if (isinstance(value, int)):
+            print("{}".format(value), end="\n")
+            status = 1
+        else:
+            raise TypeError
+    except TypeError:
+        pass
+    return status
