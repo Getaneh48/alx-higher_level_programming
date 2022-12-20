2d837b3e41daa4078f171531feaa23c0780a35bf
Getaneh48 <getaneh11@gmail.com>
2022-12-20 16:59:42 +0000

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+
 class Square:
-    """Square Class"""
+    """Represents a square class"""
     def __init__(self, size):
+        """Initialize a Square Object.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
