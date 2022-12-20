6a07a69c4349a1a51c6b250b3309ae451cca008e
Getaneh48 <getaneh11@gmail.com>
2022-12-20 18:03:28 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square class"""
+
+    def __init__(self, size=0):
+        '''
+            Initialize a Square Object.
+
+        @self:
+            refers to the class instance.
+
+        @size:
+            The size of the square must be +ve integer.
+        '''
+        self.size = size
+
+    @size.setter
+    def size(self, value):
+
+        '''
+            sets a size attribute to a given value.
+
+        @self:
+            refers to the class instance.
+
+        @value:
+            integer value to be set to the size attrib.
+        '''
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        '''
+            Returns the area of the square.
+
+        @self:
+            Refers to the class instance.
+        '''
+        return self.__size * self.__size
