a9ad64f39f73716c65dbb3ae8cb89d143a6f3b26
Getaneh48 <getaneh11@gmail.com>
2023-01-01 21:22:29 +0300

    task 100

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""a function that multiplies two matrixes"""
+
+
+def matrix_mul(m_a, m_b):
+    """returns the multiplication of two matrixes"""
+    if not type(m_a) is list or not type(m_b) is list:
+        raise TypeError("m_a must be a list or m_b must be a list")
+
+    #check if the matrix is a list of lists
+
+def validate_matrix(m):
+    """check if the given matrix is valid"""
+
+    if not type(m) is list:
+        raise TypeError("m_a must be a list or m_b must be a list")
+    for e in m:
+        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+
+    #check if the matrix is emtpy
+    if len(m) == 0:
+        raise ValueError("_a can't be empty or m_b can't be empty")
+    #check if the elements are not empty lists
+    for e in m:
+        if len(e) == 0:
+            raise ValueError("_a can't be empty or m_b can't be empty")
+
+    #check if elements of the matrix are either int or float
+    for e in m:
+       for i in e:
+           if not i is int or not i is float:
+               raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats"
+    
+    #checks if the rows of the matrix are equal
+    row_len = len(m[0])
+    for e in m:
+        if len(e) != row_len
+            raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+
