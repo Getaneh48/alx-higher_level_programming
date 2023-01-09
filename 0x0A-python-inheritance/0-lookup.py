4e66ec4372a3337c0fe11bbeb4a66b94c235e944
Getaneh48 <getaneh11@gmail.com>
2023-01-09 14:26:46 +0300

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""returns the list of available attributes and methods of an object:"""
+
+
+def lookup(obj):
+    """returns list of available atributes and methods"""
+    return dir(obj)
