d5e68b6dff9a04aecb7e8aaffedab2bf8aee2691
Getaneh48 <getaneh11@gmail.com>
2023-01-09 14:59:19 +0300

    bug fixed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,9 @@
 
 
 class MyList(list):
+    """a class that inherits from a base class list"""
+
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)"""
-        
+        # prints the list, but sorted (ascending sort)
+
         print(sorted(self))
