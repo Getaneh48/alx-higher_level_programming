b11e0906bfa52653b2d9cd27c48fb9f759dfd19f
Getaneh48 <getaneh11@gmail.com>
2023-01-10 15:45:54 +0300

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""writes an object to a text file using json formating"""
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation:"""
+
+    with open(filename, encoding="utf-8") as f:
+        json.dump(my_obj, f)
