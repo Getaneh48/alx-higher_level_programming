d3fd9b067b4622cf272425e75bf288baf8df5d9d
Getaneh48 <getaneh11@gmail.com>
2023-01-10 15:58:51 +0300

    json reading from file fixed

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,4 +7,4 @@ def load_from_json_file(filename):
     """creates an object from a json file:"""
 
     with open(filename, encoding="utf-8") as f:
-        return json.loads(f)
+        return json.load(f)
