40aa8f6737c77877e22a6e5f0f7199f9aca4198c
Getaneh48 <getaneh11@gmail.com>
2024-01-04 19:57:26 +0300

    task 1 corrected

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,10 +3,11 @@
 a program that imports functions from the file
 calculator_1.py, does some Maths, and prints the result
 """
+
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
 if __name__ == '__main__':
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
     print(f"{a} + {b} = {add(a, b)}", end="\n")
     print(f"{a} - {b} = {sub(a, b)}", end="\n")
     print(f"{a} * {b} = {mul(a, b)}", end="\n")
