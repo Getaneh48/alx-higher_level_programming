a676b60bdafb8973fedb5fb5e4d637674fc6fef1
Getaneh48 <getaneh11@gmail.com>
2024-01-04 15:06:04 +0300

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a program that imports functions from the file
+calculator_1.py, does some Maths, and prints the result
+"""
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {add(a, b)}", end="\n")
+    print(f"{a} - {b} = {sub(a, b)}", end="\n")
+    print(f"{a} * {b} = {mul(a, b)}", end="\n")
+    print(f"{a} / {b} = {div(a, b)}", end="\n")
