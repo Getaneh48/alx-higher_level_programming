c4dcfcade8cc00da2b213cfcc99c0275b705b285
Getaneh48 <getaneh11@gmail.com>
2024-01-08 14:42:51 +0300

    error fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ a function that prints a matrix of integers.
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
-            print("{}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end=" ")
         print(end="\n")
