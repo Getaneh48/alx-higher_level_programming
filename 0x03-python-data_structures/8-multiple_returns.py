89fb1bd830eeeb471092c8d40b1d9b407cb5cb7c
Getaneh48 <getaneh11@gmail.com>
2024-01-08 15:06:28 +0300

    output corrected

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,4 +6,7 @@ and its first character.
 
 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        return (len(sentence), None)
+    else:
+        return (len(sentence), sentence[0])
