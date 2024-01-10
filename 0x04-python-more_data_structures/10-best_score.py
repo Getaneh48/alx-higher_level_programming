107af4a8be0d6ea0fc2a3934b994bb38b92f2244
Getaneh48 <getaneh11@gmail.com>
2024-01-10 17:13:48 +0300

    empty dictionary test add

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,6 +6,6 @@ biggest integer value.
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary):
         return None
     return max(a_dictionary)
