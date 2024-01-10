9749bc8b73b83993ec64e9e73640780ee80c47d5
Getaneh48 <getaneh11@gmail.com>
2024-01-10 17:17:24 +0300

    error fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,6 +6,6 @@ biggest integer value.
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     return max(a_dictionary)
