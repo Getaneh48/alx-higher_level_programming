62c47047e374adeceaf5b14db73b09f178a4e685
Getaneh48 <getaneh11@gmail.com>
2024-01-23 14:47:45 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a module that defines a Square class
+"""
+
+
+class Square:
+    """
+    a class with a private instance attribute and
+    checks for the size type and value
+    """
+    def __init__(self, size=0):
+        if size and (isinstance(size, int) is False):
+            raise TypeError("size must be an integer")
+        if size and size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
