cc4a23469936087463350a35593a2a05826e5cef
Getaneh48 <getaneh11@gmail.com>
2024-01-23 14:33:39 +0300

    a square class with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a module that defines a Square class
+"""
+
+
+class Square:
+    """
+    a class with a private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
