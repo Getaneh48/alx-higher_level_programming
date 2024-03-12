40051ba4f3227d64ab050aabee548bb2bb5b1790
Getaneh48 <getaneh11@gmail.com>
2024-03-12 19:12:33 +0000

    task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- a script that lists all records of the table second_table of the database hbtn_0c_0
+--     Don’t list rows without a name value
+--     Results will display the score and the name (in this order)
+--     Records will be listed by descending score
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
