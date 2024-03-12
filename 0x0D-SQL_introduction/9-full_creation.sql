7946bd4a3dbd7ffc8dc88cb6ffc9147683e70fd1
Getaneh48 <getaneh11@gmail.com>
2024-03-12 17:40:04 +0000

    a script that creates a table and add multiple rows to it

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- a script that creates a table second_table in the database hbtn_0c_0
+--    second_table description:
+--        id INT
+--        name VARCHAR(256)
+--        score INT
+-- If the table second_table already exists, the script should not fail
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, "John", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "George", 8)
