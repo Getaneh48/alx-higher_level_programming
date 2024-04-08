36b8beb118e85bb2b0dfee2cbc4af407d7b79025
Getaneh48 <getaneh11@gmail.com>
2024-04-08 12:13:40 +0000

    a script that prints two arguments passed to it

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * a script that prints two arguments passed to it, in the following format: " is "
+*/
+
+const args = process.argv;
+
+console.log(`${args[2]} is ${args[3]}`);
