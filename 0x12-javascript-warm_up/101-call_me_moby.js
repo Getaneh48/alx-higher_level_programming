5d86cc5e9e993dd5863d833a839415ec19a27b77
Getaneh48 <getaneh11@gmail.com>
2024-04-09 01:27:12 +0000

    added a function that executes x times a function

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * a script that executes a function x times
+*/
+
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
