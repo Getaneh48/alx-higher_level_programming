cac2bac055c57fb94def21c1852b4a20c97a1276
Getaneh48 <getaneh11@gmail.com>
2024-04-26 20:06:40 +0000

    output format fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,10 +14,7 @@ if __name__ == '__main__':
     headers = {'X-GitHub-Api-Version': '2022-11-28'}
 
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        for commit in response.json():
-            commit_hash = commit['commit']['tree']['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{commit_hash}: {author_name}")
-    else:
-        print("None")
+    for commit in response.json():
+        commit_hash = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{commit_hash}: {author_name}")
