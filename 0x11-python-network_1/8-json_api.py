359d9e93423732b5059d86e2f82e5171f17dc136
Getaneh48 <getaneh11@gmail.com>
2024-04-26 18:30:55 +0000

    makes a get request and parse a json result

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if len(sys.argv) == 2:
+        search = sys.argv[1]
+        data = {'q': search}
+    else:
+        data = {'q': ''}
+    resp = requests.post(url, data=data)
+    result = resp.json()
+    if len(result) == 0:
+        print("No result")
+    else:
+        print(f"[{result['id']}] {result['name']}")
