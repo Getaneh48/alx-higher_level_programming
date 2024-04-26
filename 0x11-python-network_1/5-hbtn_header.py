186cf0d316ab3495800ab58dc1e440468135b512
Getaneh48 <getaneh11@gmail.com>
2024-04-26 17:25:54 +0000

    added a logic to get a header value from an http request using the requests module

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    resp = requests.get(url)
+    print(resp.headers.get('X-Request-Id'))
